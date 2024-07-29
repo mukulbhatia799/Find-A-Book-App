3d3dc070283c981d240f5dad157b6aaf9b47451f
Mukul Bhatia <mukulbhatia189@gmail.com>
2024-07-29 18:33:28 +0530

    'delete book' completed

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,21 +1,28 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Loading from "./Loading";
 
 export default function DeleteBook() {
   const { id } = useParams();
-  console.log("delete book id: ", id);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  <Loading />;
-  axios
-    .delete(`http://localhost:5555/books/deletebook/${id}`)
-    .then((res) => {
-      console.log(res);
-      navigate("/");
-    })
-    .catch((err) => {
-      alert("error while deleting/fetching: ", err);
-      navigate("/");
-    });
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/books/deletebook/${id}`)
+      .then((res) => {
+        setLoading(false);
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert("error while deleting/fetching: ", err);
+        navigate("/");
+      });
+  }, []);
+
+  return <>{loading == true ? <Loading /> : <div>Hello</div>}</>;
 }
